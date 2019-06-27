ba3812169c309903d136cd90309fe54504c9fa20
IMJOK <462946278@qq.com>
2019-06-27 13:57:23 +0800

    修改密码代码

--- PMS/ChangeKey1.aspx.cs
+++ PMS/ChangeKey1.aspx.cs
@@ -12,37 +12,34 @@ namespace PMS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-           
         }
 
 
         protected void Button1_Click1(object sender, EventArgs e)
         {
             String eid = Session["eid"].ToString();
-            Response.Write(eid);
-            Response.Write("eid");
             String pwd = TextBox_old.Text.ToString().Trim();
             // 连接数据库，查询密码是否正确
             string sqlconn = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
                    "AttachDbFilename='|DataDirectory|\\Database1.mdf';";
             using (SqlConnection cn = new SqlConnection())
             {
-                cn.ConnectionString = sqlconn; cn.Open();
-                string sqlstr = string.Format("SELECT * FROM [员工]");
-                SqlCommand cmd = new SqlCommand(sqlstr, cn);
-                SqlDataReader reader = cmd.ExecuteReader();//执行查找指令并返回查找结果
-                while (reader.Read())
-                {
-                    if (reader["eid"].ToString().Replace(" ", "") == eid && reader["password"].ToString().Replace(" ", "") == pwd)
-                    {
+                cn.ConnectionString = sqlconn;
+                cn.Open();
+                //SqlDataReader reader = cmd.ExecuteReader();//执行查找指令并返回查找结果
+                //while (reader.Read())
+                //{
+                    //if (reader["eid"].ToString().Replace(" ", "") == eid && reader["password"].ToString().Replace(" ", "") == pwd)
+                    //{
                         // 用HttpUtility解决中文乱码问题
                         HttpCookie cookiePwd = new HttpCookie("eid", HttpUtility.UrlEncode(eid));
                         Response.AppendCookie(cookiePwd);
                         if (TextBox_new.Text.Equals(TextBox_new2.Text))//检查新密码输入是否一致
                         {//编写sql语句实现修改密码
                             string sql = string.Format(" update 员工 set password =N'{0}' where eid ='{1}'", TextBox_new.Text, eid);
-                            
+                        SqlCommand cmd = new SqlCommand(sql, cn);
+                        int effectLine = cmd.ExecuteNonQuery();
+                        Label_err.Text = "修改成功！";
                         }
                         else
                         {
@@ -52,18 +49,18 @@ namespace PMS
                             Label_err.Text = "请检查输入！";
                         }
 
-                    }
-                    else
-                    {
-                        TextBox_old.Text = "";
-                        TextBox_new2.Text = "";
-                        TextBox_new.Text = "";
-                        Label_err.Text = "密码错误！";
-                    }
+                    //}
+                    //else
+                    //{
+                    //    TextBox_old.Text = "";
+                    //    TextBox_new2.Text = "";
+                    //    TextBox_new.Text = "";
+                    //    Label_err.Text = "密码错误！";
+                    //}
 
                 }
                 
-            }
+            //}
         }
     }
 }
